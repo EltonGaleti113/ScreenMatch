76ff76eb302571d6c60ea9de321ff4fa719b1ec8
EltonGaleti113 <egaleti2003@gmail.com>
2024-02-04 16:59:40 -0300

    Commit Inicial

---
+++ src/Programa.java
@@ -0,0 +1,11 @@
+public class Programa {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+
+        meuFilme.nome = "Samanbaia no Fogo";
+        meuFilme.notaDeAvaliacao = 8;
+        System.out.println(meuFilme.nome);
+        System.out.println(meuFilme.notaDeAvaliacao);
+
+    }
+}
